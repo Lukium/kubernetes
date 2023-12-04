2be63ee434e93b9eab80b1234857b39dfe2ad043
Lukium <99280463+Lukium@users.noreply.github.com>
2023-12-04 09:46:58 -0500

    Update cluster-wipe.sh

--- k3s/cluster-wipe.sh
+++ k3s/cluster-wipe.sh
@@ -138,3 +138,8 @@ if [ "$use_ssh_passphrase" = true ] ; then
     echo "Killing ssh-agent with PID: $SSH_AGENT_PID"
     kill $SSH_AGENT_PID
 fi
+
+echo -e "\e[32mAll nodes wiped\e[0m"
+
+# Unset all variables
+unset use_ssh_passphrase copy_ssh_id cert_name user nodes masters agents node ssh_agent_output SSH_AGENT_PID
